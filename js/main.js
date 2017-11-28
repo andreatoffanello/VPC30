85e71e76a78e81a63af7d01f641a67aeb561abe8
Andrea Toffanello <andrea.toffanello@gmail.com>
2017-11-28 19:12:20 +0100

    prima fase creazione menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-//var functionBasedDelay = anime({
-//  targets: '.menu_item',
-//  translateY: -50,
-//  delay: function(el, i, l) {
-//    return i * 100;
-//  }
-//});
+var menuTimeline = anime.timeline({
+	autoplay: false,
+	direction: 'reverse'
+});
 
-var animationsTimeline = anime.timeline();
-
-animationsTimeline
+menuTimeline
+.add({
+    targets: '.fullmenu .column.is-half',
+    width: '50%',
+    padding: '0.75rem',
+    easing: 'easeOutQuart',
+    delay: function(el, i, l) {
+	    return i * 300;
+	}
+})
 .add({
     targets: '.menu_item',
-    translateY: -50,
-    easing: 'linear',
+    translateY: {
+    	duration: 1000,
+    	value: [100, 0]
+    },
+    opacity: 1,
+    easing: 'easeOutQuart',
+    offset: '-=600',
 	delay: function(el, i, l) {
-	    return i * 100;
+	    return i * 200;
 	}
+})
+.add({
+    targets: '.menu_title',
+    left: '0',
+    opacity: '0.05',
+    easing: 'easeOutQuart',
+    duration: 600,
+    offset: '-=1600'
+})
+
+.add({
+    targets: '.menu_title',
+    left: '50px',
+    easing: 'easeOutQuart',
+    duration: 30000,
+    loop: true,
+    direction: 'alternate',
+    offset: '-=900'
 });
+
+document.querySelector('.menu_toggle a').onclick = function() {
+  menuTimeline.play();
+  menuTimeline.reverse();
+};
